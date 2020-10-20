2777fb24092abb3ce0c7a7f25a8645b2b3d01d2c
killermitch <anthonnymitch22@gmail.com>
2020-10-20 19:08:27 -0400

    server.js created

---
+++ Db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger',true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beef Burger',true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Chicken Burger ', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fish Burger', true);
